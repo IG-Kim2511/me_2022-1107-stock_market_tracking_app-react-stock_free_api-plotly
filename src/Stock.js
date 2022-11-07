e0275a11f25c3d28b4aaafae23c6abb350689991
IG <iikim2511@gmail.com>
2022-11-07 16:04:21 -0500

    stockChart_X_value

--- src/Stock.js
+++ src/Stock.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Stock = () => {
   let [stockChart_X_value, setStockChart_X_value] = useState([]);
   let [stockChart_Y_value, setStockChart_Y_value] = useState([]);
+
+  let stockChart_X_value_function =[];
+  let stockChart_Y_value_function =[];
 
   // 🍀env 파일은 node와 연결할때만 사용가능함 (react 단독으로 불가능)
   const API_KEY = "HGJWFG4N8AQ66ICD";
@@ -12,10 +16,39 @@ const Stock = () => {
   console.log(API_Call)
 
 
+  useEffect(() => {
+    const fetchItems = async () => {
+        const result = await axios(API_Call);
+  
+        console.log(result.data);
+ 
+        for (let key in result.data['Time Series (Daily)']) {
+            stockChart_X_value_function.push(key);
+            stockChart_Y_value_function.push(result.data['Time Series (Daily)'][key]['4. close']);
+        }
+        console.log(stockChart_X_value_function)
+        console.log(stockChart_Y_value_function)
+        setStockChart_X_value( stockChart_X_value_function);
+        setStockChart_Y_value( stockChart_Y_value_function);
+        
+    };        
+    fetchItems();
+}, [])
 
+
+    console.log(stockChart_X_value)
+    console.log(stockChart_Y_value)
+
+  
   return (
     <div>
       <h1>Stock market</h1>
+      {/* <div>{stockChart_X_value}</div> */}
+      {/* <div>{stockChart_Y_value}</div> */}
+
+
+
+
     </div>
   );
 };
